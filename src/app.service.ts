3cc78bfdeb0a24de82fad0423aed5d7a2f49263b
GabrielVanderlinde-Qualle <gabriel.vanderlinde@quallecontrol.com.br>
2026-05-14 11:59:08 -0300

    feat: formata projeto e tabem finaliza swagger inicial

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
   getHealtCheck() {
-    return {message: "API is running"};
+    return { message: 'API is running' }
   }
 }
