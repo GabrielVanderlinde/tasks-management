ae01efa4faca2e27932b708e3d8375613934c86d
GabrielVanderlinde-Qualle <gabriel.vanderlinde@quallecontrol.com.br>
2026-05-14 09:38:03 -0300

    feat: adiciona suporte ao Swagger e método de HealthCheck na API

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import { VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  //Versionamento
   app.enableVersioning({
     type: VersioningType.URI,
   })
+
+  //Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Curso NestJS - Tasks API')
+    .setDescription('API desenvolvida durante o curso de NestJS - Monaro Dev')
+    .setVersion('1')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
 
   await app.listen(process.env.PORT ?? 3005)
 }
