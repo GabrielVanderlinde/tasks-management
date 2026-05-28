7432f238e036b61ea89dfc12950c748b5f7dd30b
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-05-28 14:21:47 -0300

    feat: implementa classe service

--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  findAll() {
+    return ['teste 1', 'teste 2', 'teste 3']
+  }
+
+  findById(id: string) {
+    return 'teste 1'
+  }
+
+  create(data: any) {
+    return 'create teste 1'
+  }
+
+  update(id: string, data: any) {
+    return 'update teste 1'
+  }
+
+  remove(id: string) {
+    return 'remove teste 1'
+  }
+}
