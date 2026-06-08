abf55b3d1a92150fd076ed95dd26b4a2247de930
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-06-08 17:59:28 -0300

    feat: implementa response no swagger

--- src/modules/projects/projects.controller.ts
+++ src/modules/projects/projects.controller.ts
@@ -1,6 +1,18 @@
-import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common'
-import { ProjectsRequestDto } from './projects.dto'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common'
+import { ProjectsListItemDTO, ProjectsRequestDto } from './projects.dto'
 import { ProjectsService } from './projects.service'
+import { ApiResponse } from '@nestjs/swagger'
 
 @Controller({
   version: '1',
@@ -10,26 +22,31 @@ export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Get()
+  @ApiResponse({ type: [ProjectsListItemDTO] })
   findAll() {
     return this.projectsService.findAll()
   }
 
   @Get(':id')
+  @ApiResponse({ type: ProjectsListItemDTO })
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.projectsService.findById(id)
   }
 
   @Post()
+  @ApiResponse({ type: ProjectsListItemDTO })
   create(@Body() data: ProjectsRequestDto) {
     return this.projectsService.create(data)
   }
 
   @Put(':id')
+  @ApiResponse({ type: ProjectsListItemDTO })
   update(@Param('id', ParseUUIDPipe) id: string, @Body() data: ProjectsRequestDto) {
     return this.projectsService.update(id, data)
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.projectsService.remove(id)
   }
--- src/modules/projects/projects.dto.ts
+++ src/modules/projects/projects.dto.ts
@@ -15,3 +15,20 @@ export class ProjectsRequestDto {
   @IsString()
   description!: string
 }
+
+export class ProjectsListItemDTO {
+  @ApiProperty({ description: 'Project id', example: 'uuid' })
+  id!: string
+  @ApiProperty({ description: 'Project name', example: 'Project 1' })
+  name!: string
+  @ApiProperty({
+    description: 'Project description',
+    example: 'This is a project',
+    required: false,
+  })
+  description!: string
+  @ApiProperty({ description: 'Project created at', example: '2023-01-01T00:00:00.000Z' })
+  createdAt!: string
+  @ApiProperty({ description: 'Project updated at', example: '2023-01-01T00:00:00.000Z' })
+  updatedAt!: string
+}
