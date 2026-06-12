3f6651de7d232826ec520c1e9470a5ea46fa11e0
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-06-12 17:31:52 -0300

    feat: cria task controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ProjectsModule } from './modules/projects/projects.module'
 import { TasksModule } from './modules/tasks/tasks.module'
-import { PrismaService } from './prisma.service'
+
 
 @Module({
   imports: [ProjectsModule, TasksModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
